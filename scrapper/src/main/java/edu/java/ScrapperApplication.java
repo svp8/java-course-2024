9cb53e7d8ddc19d232d94efbb3123c314342e87e
svp8 <azaz08082001@gmail.com>
2024-03-06 19:26:44 +0300

    hw4 fix

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,13 +1,10 @@
 package edu.java;
 
-import edu.java.configuration.SchedulerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SchedulerConfiguration.class)
 @EnableScheduling
 public class ScrapperApplication {
     public static void main(String[] args) {
