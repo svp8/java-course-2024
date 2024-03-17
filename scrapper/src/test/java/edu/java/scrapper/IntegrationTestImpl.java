ad5be57607fce39ef58ba9ef2e48d9fe0f6bca81
svp8 <azaz08082001@gmail.com>
2024-03-17 20:37:52 +0300

    hw4 fix

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTestImpl.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTestImpl.java
@@ -2,6 +2,7 @@ package edu.java.scrapper;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Assertions;
@@ -12,21 +13,29 @@ public class IntegrationTestImpl extends IntegrationTest {
     @Test
     @DisplayName("Check if tables from migrations created")
     void testMigrations() throws SQLException {
-        try (Connection connection = DriverManager.getConnection(
-            POSTGRES.getJdbcUrl(),
-            POSTGRES.getUsername(),
-            POSTGRES.getPassword()
-        )) {
-            ResultSet result = connection.prepareStatement("""
-                select count(*) FROM information_schema.tables
-                                WHERE  table_schema = 'public'
-                                 AND    table_name   = 'link' or  table_name   = 'chat'
-                                 or  table_name   = 'chat_link'
-                                ;
-                """).executeQuery();
+        try (
+            Connection connection = DriverManager.getConnection(
+                POSTGRES.getJdbcUrl(),
+                POSTGRES.getUsername(),
+                POSTGRES.getPassword()
+            );
+            PreparedStatement preparedStatement = connection.prepareStatement("""
+                select
+                  count(*)
+                FROM
+                  information_schema.tables
+                WHERE
+                  table_schema = 'public'
+                  AND table_name = 'link'
+                  or table_name = 'chat'
+                  or table_name = 'chat_link';
+                """)
+        ) {
+            ResultSet result = preparedStatement.executeQuery();
             while (result.next()) {
                 Assertions.assertEquals(3, result.getInt(1));
             }
+
         }
 
     }
