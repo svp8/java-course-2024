1f1002a72747add288faa33a666448807d92c639
svp8 <azaz08082001@gmail.com>
2024-03-20 22:16:21 +0300

    hw5 fixed after merge

--- scrapper/src/main/java/edu/java/client/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/client/StackOverflowClient.java
@@ -4,7 +4,7 @@ import edu.java.dto.stack.AnswerDto;
 import edu.java.dto.stack.CommentDto;
 import edu.java.dto.stack.GeneralResponse;
 
-public interface StackOverflowClient extends Client{
+public interface StackOverflowClient  {
     GeneralResponse<AnswerDto> getAnswersByQuestionId(int id);
 
     GeneralResponse<CommentDto> getCommentsByQuestionId(int id);
